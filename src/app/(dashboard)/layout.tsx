3dc9ce9d06791020eb49975bdf994a3c33ca49b2
Elameen Daiyabu <elameendaiyabu@proton.me>
2026-02-08 10:57:16 +0100

    removed isManager prop

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -34,7 +34,6 @@ export default async function RootLayout({
             employeeId={authData.employee.id}
             employeeRole={authData.employee.role}
             employeeDepartment={authData.employee.department}
-            employeeIsManager={!!authData.employee.isManager}
           />
           <SidebarInset>
             <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
